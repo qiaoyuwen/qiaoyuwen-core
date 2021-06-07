e6f72ad63a22fa88ba6d116b52bcc410364e7f0a
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-06-07 18:26:56 +0800

    feat: add useSelection

---
+++ packages/react-hooks/src/selection/demo/useSelection.tsx
@@ -0,0 +1,32 @@
+import type { FunctionComponent } from 'react';
+import { Space, Checkbox } from 'antd';
+import 'antd/dist/antd.css';
+import { useSelection } from '..';
+
+const Component: FunctionComponent = () => {
+  const data = [1, 2, 3, 4];
+  const defaltSelections = [1, 2];
+  const [, { isSelected, toggle }, { allSelected, partiallySelected, toggleAll }] = useSelection(
+    data,
+    defaltSelections,
+  );
+
+  return (
+    <>
+      <Space direction="vertical">
+        {data.map((item) => (
+          <Checkbox checked={isSelected(item)} onChange={() => toggle(item)}>
+            {item}
+          </Checkbox>
+        ))}
+        <Space>
+          <Checkbox indeterminate={partiallySelected} onChange={toggleAll} checked={allSelected}>
+            全选
+          </Checkbox>
+        </Space>
+      </Space>
+    </>
+  );
+};
+
+export default Component;
