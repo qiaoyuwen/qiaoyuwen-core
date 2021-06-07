6b148df18caf0c04b28741adf1862d7e14977e22
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-06-07 17:27:03 +0800

    feat: add useArray doc

---
+++ packages/react-hooks/src/array/demo/useArray.tsx
@@ -0,0 +1,36 @@
+import type { FunctionComponent } from 'react';
+import { Space, Button, List } from 'antd';
+import 'antd/dist/antd.css';
+import { useArray } from '..';
+
+const Component: FunctionComponent = () => {
+  const [data, { push, remove, reverse }] = useArray<string>([]);
+
+  return (
+    <>
+      <Space direction="vertical">
+        <List
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <span style={{ width: 250 }}>{item}</span>
+              <Button type="primary" danger onClick={() => remove(item)}>
+                删除
+              </Button>
+            </List.Item>
+          )}
+        />
+        <Space>
+          <Button type="primary" onClick={() => push(`${Math.random()}`)}>
+            添加
+          </Button>
+          <Button type="primary" onClick={() => reverse()}>
+            反转
+          </Button>
+        </Space>
+      </Space>
+    </>
+  );
+};
+
+export default Component;
