133bc89d28e16d3d8ab58c507e921d3b2f6f19e4
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-06-17 10:56:42 +0800

    feat: add useCountdown doc

---
+++ packages/react-hooks/src/timeout/demo/useCountdown.tsx
@@ -0,0 +1,30 @@
+import type { FunctionComponent } from 'react';
+import React from 'react';
+import 'antd/dist/antd.css';
+import { useCountdown } from '..';
+import { Button, Space } from 'antd';
+
+const Component: FunctionComponent = () => {
+  const [countdown, start, pause, reset] = useCountdown(60 * 1000, 1 * 1000);
+
+  return (
+    <>
+      <Space direction="vertical">
+        <span>倒计时: {countdown / 1000}s</span>
+        <Space>
+          <Button type="primary" onClick={() => start()}>
+            开始
+          </Button>
+          <Button type="primary" onClick={() => pause()}>
+            暂停
+          </Button>
+          <Button type="primary" onClick={() => reset()}>
+            重置
+          </Button>
+        </Space>
+      </Space>
+    </>
+  );
+};
+
+export default Component;
---
+++ packages/react-hooks/src/timeout/useCountdown.md
@@ -0,0 +1,31 @@
+---
+title: useCountdown
+nav:
+  title: Hooks
+  path: /hook
+order: 1
+---
+
+# useCountdown
+
+管理倒计时的 hook
+
+<code src="./demo/useCountdown.tsx">
+
+# API
+
+```typescript
+const [countdown, start, pause, reset] = useCountdown(
+  initialCountdownMs: number,
+  stepMs: number,
+);
+```
+
+# 返回值
+
+| 参数      | 说明   | 类型       |
+| --------- | ------ | ---------- |
+| countdown | 倒计时 | number     |
+| start     | 启动   | () => void |
+| pause     | 暂停   | () => void |
+| reset     | 重置   | () => void |
