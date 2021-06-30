514e20bc6c98466904673db8b4da11ae5913a046
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-06-30 15:50:40 +0800

    feat: hash

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'React hooks',
   mode: 'site',
+  history: {
+    type: "hash",
+  },
   publicPath: process.env.CI ? "/react-hooks/" : "/",
   // more config: https://d.umijs.org/config
 });
