efff4d5d32a81da94af646feffbd3051d0184518
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-06-30 15:40:52 +0800

    feat: .umirc

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'React hooks',
   mode: 'site',
+  publicPath: process.env.CI ? "/react-hooks/" : "/",
   // more config: https://d.umijs.org/config
 });
